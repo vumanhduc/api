bd5c299b31e06d11b4f5c62163e12ed59e8f3f99
Vũ Mạnh Đức <ducvm.18@gmail.com>
2018-04-13 16:30:33 +0700

    Modified Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -15,12 +15,17 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'name' => $this->,
+            'name' => $this->name,
             'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock,
-            'discount' => $this->discount
+            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'discount' => $this->discount,
+            'totalPrice' => round((1 - ($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() >0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index',$this->id)
+            ]
 
-        ]
+        ];
     }
 }
